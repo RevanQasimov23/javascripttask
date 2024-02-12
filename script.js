ef41a476406285eadd18f52a80c7a285287927d6
RevanQasimov23 <qasimovnicat16@gmail.com>
2024-02-12 16:36:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function Toggle(){
+    let heading = document.querySelector("h4")
+    if(heading.style.display !="none"){
+       heading.style.display = "none";
+    }
+    else{
+      heading.style.display = "block";  
+    }
+    }
+
+    function Toggle1(){
+        let heading = document.querySelector("h3")
+        if(heading.style.display !="none"){
+           heading.style.display = "none";
+        }
+        else{
+          heading.style.display = "block";  
+        }
+        }
+
+        function Toggle2(){
+            let heading = document.querySelector("h5")
+            if(heading.style.display !="none"){
+               heading.style.display = "none";
+            }
+            else{
+              heading.style.display = "block";  
+            }
+            }
+
+            function Toggle3(){
+                let heading = document.querySelector("p")
+                if(heading.style.display !="none"){
+                   heading.style.display = "none";
+                }
+                else{
+                  heading.style.display = "block";  
+                }
+                }
